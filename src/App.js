c134d1121fd73d03b7e3b07a416f79a2d6799fc6
dorukiskk <doruk003@gmail.com>
2024-03-08 12:36:22 +0300

    sign up created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import Login from './screens/Login';
+import SignUp from "./screens/SignUp";
 
 function App() {
   return (
     <div className='bg-main-bg h-screen'>
-      <Login></Login>
+        {/*<Login></Login>*/}
+        <SignUp></SignUp>
     </div>
   );
 }
