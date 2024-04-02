7bdaeec74ef5802c12cda2dd9cb2dfc8964493ed
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-04-02 21:50:00 +0300

    puşt

---
+++ backend/routes/logRegRoute.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// Get all users
+
+router.get('/getUsers', (req, res) => {
+    
+});
+
+router.get('/sign-up', (req, res) => {
+    res.send("Whaduuuuo");
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 
 // routers
-const userRouter = require('./routes/user_router');
+const userRouter = require('./routes/user_route');
+const logRegRoute = require('./routes/logRegRoute');
 
 
 app.use(express.json());
@@ -18,3 +19,4 @@ app.use((err, req, res, next) => {
 });
 
 app.use('/user', userRouter);
+app.use('/', logRegRoute);
