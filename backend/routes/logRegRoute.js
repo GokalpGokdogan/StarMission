a756a8b2e396b6707cce9bcfb67985bc0dfea69a
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-04-06 08:27:48 +0300

    register Astronaut / Company finished

--- backend/routes/logRegRoute.js
+++ backend/routes/logRegRoute.js
@@ -13,12 +13,29 @@ router.get('/sign-up', (req, res) => {
     res.send("Whaduuuuo");
 });
 
-// Register
-router.post('/register', async(req, res) => {
+// Register Astronaut
+router.post('/registerAstronaut', async(req, res) => {
 
     // get user data
     try {
-        const response = await logResController.register(req.body);
+        const response = await logResController.registerAstronaut(req.body);
+        res.status(200).send(response);
+    } catch (error) {
+        if (error.code === 'ER_DUP_ENTRY') {
+            res.status(400).send("ERROR_EMAIL_EXISTS");
+        } else {
+            res.status(400).send("An error occurred: "+ error);
+        }
+    }
+    
+});
+
+// Register Company
+router.post('/registerCompany', async(req, res) => {
+
+    // get user data
+    try {
+        const response = await logResController.registerCompany(req.body);
         res.status(200).send(response);
     } catch (error) {
         if (error.code === 'ER_DUP_ENTRY') {
