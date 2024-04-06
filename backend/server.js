54aa205c81479b967cd6bdab7e87942c43da8a61
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-04-06 08:46:20 +0300

    merged backend-gg and backend-tefo

--- backend/server.js
+++ backend/server.js
@@ -2,24 +2,28 @@ const express = require('express');
 const app = express();
 
 // routers
-const userRouter = require('./routes/user_route');
+
 const logRegRoute = require('./routes/logRegRoute');
+const manageEmployeesRoute = require('./routes/company/manageEmployeesRoute');
 
 
 app.use(express.json());
+
 app.use((req, res, next) => {
     console.log(req.path, req.method);
     next();
 });
-
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
-app.use('/user', userRouter);
+
+// routes
+
 app.use('/logReg', logRegRoute);
+app.use('/manageEmployees', manageEmployeesRoute);
 
 app.listen(3001, () => {
     console.log('Server is running on port 3001');
