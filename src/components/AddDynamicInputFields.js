d45576e7d81eee32443bf439e4453c2c8c12ff62
Doruk Isik <doruk.iskk@gmail.com>
2024-04-08 22:00:00 +0300

    Create Mission Screen update

--- src/components/AddDynamicInputFields.js
+++ src/components/AddDynamicInputFields.js
@@ -34,17 +34,20 @@ function AddDynamicInputFields() {
                             onChange={(event) => handleChange(event, index)}
                         />
                         {inputs.length > 1 && (
-                            <button onClick={() => handleDeleteInput(index)} className="ml-2">Delete</button>
+                            <svg onClick={() => handleDeleteInput(index)} className="h-6 w-6 text-button-red transition-colors duration-300 hover:text-red-600 hover:scale-105 cursor-pointer"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="4" y1="7" x2="20" y2="7" />  <line x1="10" y1="11" x2="10" y2="17" />  <line x1="14" y1="11" x2="14" y2="17" />  <path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" />  <path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
                         )}
                     </div>
                     <div>
                         {index === inputs.length - 1 && (
-                            <button onClick={() => handleAddInput()} className="ml-2">Add</button>
+                            <svg onClick={() => handleAddInput()} className="h-8 w-8 text-button-purple cursor-pointer transition-colors duration-300 hover:scale-105 ease-in-out hover:text-indigo-700" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                                <circle cx="12" cy="12" r="10" />
+                                <line x1="12" y1="8" x2="12" y2="16" />
+                                <line x1="8" y1="12" x2="16" y2="12" />
+                            </svg>
                         )}
                     </div>
                 </div>
             ))}
-            <div className="body"> {JSON.stringify(inputs)} </div>
         </div>
 
     );
