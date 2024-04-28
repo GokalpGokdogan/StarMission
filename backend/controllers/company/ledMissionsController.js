77664ef76da22cf60e0a4b308f71c49875ee84e4
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-04-28 17:54:08 +0300

    query update on led missions

--- backend/controllers/company/ledMissionsController.js
+++ backend/controllers/company/ledMissionsController.js
@@ -5,13 +5,14 @@ const getLedMissions = async (data) => {
         
     const { companyId , name, start_date, end_date, location, min_budget, max_budget} = data;
     let query = `SELECT DISTINCT * FROM space_mission s
-                WHERE s.leading_firm_id = ? ORDER BY s.end_date
+                WHERE s.leading_firm_id = ? AND s.end_date > CURDATE()
                 AND (CASE WHEN ? IS NOT NULL THEN s.name LIKE ? ELSE 1 END)
                 AND (CASE WHEN ? IS NOT NULL THEN s.start_date >= ? ELSE 1 END) 
                 AND (CASE WHEN ? IS NOT NULL THEN s.end_date <= ? ELSE 1 END)
                 AND (CASE WHEN ? IS NOT NULL THEN s.location = ? ELSE 1 END)
                 AND (CASE WHEN ? IS NOT NULL THEN s.budget >= ? ELSE 1 END)
-                AND (CASE WHEN ? IS NOT NULL THEN s.budget <= ? ELSE 1 END);`;
+                AND (CASE WHEN ? IS NOT NULL THEN s.budget <= ? ELSE 1 END)
+                ORDER BY s.end_date;`;
         db.query(query, [companyId, name, start_date, end_date, location, min_budget, max_budget], (err, result) => {
                 if (err) {
                     reject(err);
