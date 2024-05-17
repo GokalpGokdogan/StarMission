ebbe1ffaa8d25f9c7925f59ee630b9d803e28450
Doruk Isik <doruk.iskk@gmail.com>
2024-05-17 23:06:36 +0300

    error handling for create mission / redirecting

--- frontend/src/screens/company/CreateMission.js
+++ frontend/src/screens/company/CreateMission.js
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import AddDynamicInputFields from "../../components/AddDynamicInputFields";
 import { createMission } from "../../Requests";
 import Header from '../../components/Header';
+import Alert from '@mui/material/Alert';
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 import { useUser } from "../../UserProvider";
 
 const CreateMission = () => {
@@ -14,6 +17,8 @@ const CreateMission = () => {
     const [important_notes, setImportantNotes] = useState('');
     const [inputs, setInputs] = useState([{ note: "" }]);
     const {userId} = useUser();
+    const [showAlert, setShowAlert] = useState(false);
+    const [alertText, setAlertText] = useState('');
 
     const getJoinedString = () => {
         // Extract values efficiently using map
@@ -42,6 +47,34 @@ const CreateMission = () => {
         setDescription(e.target.value);
     };
 
+    const handleCreateMission = async (e) => {
+        try{
+            e.preventDefault(); // Prevent default form submission behavior
+            await createMission(userId, name, location, start_date, end_date, description, 0, important_notes);
+                     
+            setAlertText('Create mission successful! Redirecting to leading missions...');
+            setShowAlert(true);
+
+            const redirectUrl = `/leading-missions`;
+            setTimeout(() => {
+                window.location.href = redirectUrl;
+            }, 2000);
+    
+        } catch (error){
+            if(error.response.data == "An error occured while creating mission: MISSION NAME IS ALREADY TAKEN"){
+                setAlertText('Mission name is already taken!');
+                setShowAlert(true);
+            }
+            else if(error.response.data == "An error occured while creating mission: MISSION BUDGET EXCEEDS BALANCE"){
+                setAlertText('Mission budget exceeds balance!');
+                setShowAlert(true);
+            }
+            
+            console.error('Error creating mission:', error);
+            console.log(error.response.data);
+        }
+    };
+
 
     useEffect(() => {
         console.log(inputs);
@@ -55,8 +88,7 @@ const CreateMission = () => {
             <div className="bg-white mt-5 py-2 border rounded-xl border-transparent p-2 border-10 py-8 px-4 mx-auto max-w-2xl lg:py-16">
                 <h2 className="mb-4 text-xl font-bold text-main-text">Create New Mission</h2>
                 <form onSubmit={(e) => {
-                    e.preventDefault(); // Prevent default form submission behavior
-                    createMission(userId, name, location, start_date, end_date, description, 0, important_notes);
+                    handleCreateMission(e);
                 }}>
                     <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
                         <div className="sm:col-span-2">
@@ -91,6 +123,18 @@ const CreateMission = () => {
                     </div>
                 </form>
             </div>
+            {showAlert && (
+                <div className={`fixed bottom-4 right-4 max-w-96 flex ${alertText.length > 40 ? 'flex-col items-end justify-center' : 'flex-row items-center'}`}>
+                    <Alert severity={alertText.includes('successful') ? 'success' : 'error'} className="w-full">
+                        <div className="flex items-center justify-between w-full">
+                            <div>{alertText}</div>
+                            <IconButton onClick={() => setShowAlert(false)}>
+                                <CloseIcon />
+                            </IconButton>
+                        </div>
+                    </Alert>
+                </div>
+            )}
         </div>
     )
 }
