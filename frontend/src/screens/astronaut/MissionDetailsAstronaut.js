33b15ef43b3f8539991034c918a0d75e7dc32037
öykü demir <oykuudemirr16@gmail.com>
2024-05-17 22:20:37 +0300

    mission details astronaut minor changes

--- frontend/src/screens/astronaut/MissionDetailsAstronaut.js
+++ frontend/src/screens/astronaut/MissionDetailsAstronaut.js
@@ -105,8 +105,8 @@ useEffect(() => {
               <img width="90" height="90" src="https://seekvectorlogo.com/wp-content/uploads/2018/02/nasa-vector-logo.png" alt="NASA Logo" />
               <div>
                 <p className="text-xl font-semibold leading-5 mt-3 text-main-text">{missionData.company_name}</p>
-                <p className="truncate text-base leading-5 text-sub-text">{missionData.location}</p>
-                <p className="truncate text-sm leading-5 text-sub-text">Mission Start-End: {formatDate(missionData.start_date)} - {formatDate(missionData.end_date)}</p>
+                <p className="truncate text-base font-medium leading-5 text-sub-text">{missionData.location}</p>
+                <p className="break-all text-sm font-medium leading-5 text-sub-text">Mission Start-End: {formatDate(missionData.start_date)} - {formatDate(missionData.end_date)}</p>
               </div>
             </div>
             <div className="flex flex-col px-1 py-1 ml-8 mr-8 mt-8 mb-4 w-128 bg-grey-bg rounded-xl">
