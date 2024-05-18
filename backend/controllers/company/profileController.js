cb7f42bf835da94e0d63a25739379e070efbbbcb
Tevfik Emre Sungur <tevfikemresungur@gmail.com>
2024-05-18 15:32:38 +0300

    editprofile company update

--- backend/controllers/company/profileController.js
+++ backend/controllers/company/profileController.js
@@ -27,27 +27,27 @@ const editProfile = async (data) => {
         
         let query = ``;
         if(name){
-            query += `UPDATE user SET name = '${name}' WHERE user_id = ${companyId};`;
+            query += `UPDATE user SET name = "${name}" WHERE user_id = ${companyId};`;
         }
 
         if(email){
-            query += `UPDATE user SET email = '${email}' WHERE user_id = ${companyId};`;
+            query += `UPDATE user SET email = "${email}" WHERE user_id = ${companyId};`;
         }
 
         if(phone){
-            query += `UPDATE user SET phone = '${phone}' WHERE user_id = ${companyId};`;
+            query += `UPDATE user SET phone = "${phone}" WHERE user_id = ${companyId};`;
         }
 
         if(password){
-            query += `UPDATE user SET password = '${password}' WHERE user_id = ${companyId};`;
+            query += `UPDATE user SET password = "${password}" WHERE user_id = ${companyId};`;
         }
 
         if(foundation_date){    
-            query += `UPDATE company SET foundation_date = '${foundation_date}' WHERE user_id = ${companyId};`;
+            query += `UPDATE company SET foundation_date = "${foundation_date}" WHERE user_id = ${companyId};`;
         }
 
         if(description){
-            query += `UPDATE company SET description = '${description}' WHERE user_id = ${companyId};`;
+            query += `UPDATE company SET description = "${description}" WHERE user_id = ${companyId};`;
         }
 
         if(balance){
