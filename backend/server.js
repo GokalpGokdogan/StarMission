bda2be1ba7f8e2b092e9cb6380fce610c248df23
Gokalp Gokdogan <gokalp.gokdogan@gmail.com>
2024-05-19 16:47:45 +0300

    timeout to 0

--- backend/server.js
+++ backend/server.js
@@ -76,4 +76,4 @@ const server = app.listen(3001, () => {
     console.log('Server is running on port 3001');
 });
 
-server.timeout = 1000 * 60 * 10; // 10 minutes
+server.timeout = 1000 * 60 * 0; // 0 minutes
