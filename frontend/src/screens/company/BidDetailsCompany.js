9b072fbc63ab3297b1374ae6d20e4a74c41afb07
Doğa Elif Konuk <dogaelifkonuk@gmail.com>
2024-05-19 15:09:37 +0300

    bidde mission budget görme

--- frontend/src/screens/company/BidDetailsCompany.js
+++ frontend/src/screens/company/BidDetailsCompany.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import { Link, Route, useParams } from 'react-router-dom';
-import { getBidData, rejectIncomingBid, acceptIncomingBid, getImageById, getCompanyData} from '../../Requests';
+import { getBidData, rejectIncomingBid, acceptIncomingBid, getImageById, getCompanyData, getMissionData} from '../../Requests';
 import Alert from '@mui/material/Alert';
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
@@ -14,6 +14,7 @@ const BidDetailsCompany = () => {
   const { bidId } = useParams();
   const [bidData, setBidData] = useState({});
   const [companyData, setCompanyData] = useState({});
+  const [missionData, setMissionData] = useState({});
   const [url, setUrl] = useState(''); 
   const {userId} = useUser();
 
@@ -64,6 +65,27 @@ const BidDetailsCompany = () => {
         console.error('Error fetching companies:', error);
     }
   };
+
+  
+  const fetchMissionData = async () => {
+    try{
+        const mission = await getMissionData(bidData.mission_id);
+        if(mission == "No missions found with these filters")
+        {
+          console.log("ahah")
+        }
+        else
+        {
+          setMissionData(mission);
+          console.log(mission);
+        }
+        
+    } catch (error){
+        console.error('Error fetching missions:', error);
+    } finally {
+      setTimeout(() => 300);
+    }
+};
 
   const handleRejectIncomingBid = async () => {
     try{
@@ -116,6 +138,7 @@ const handleAcceptIncomingBid = async () => {
 
   useEffect(() => {
     fetchBidData();
+    fetchMissionData();
   }, [bidId]);
 
   return (
@@ -136,7 +159,7 @@ const handleAcceptIncomingBid = async () => {
               <div className="px-1 py-5 ml-8 mr-8 mt-4 items-center bg-grey-bg rounded-xl">
                 <p className="text-xl font-semibold leading-5 ml-3 mr-3 text-main-text">${bidData.requested_amount}</p>
               </div>
-              <h2 className="text-sm font-bold bg-bid-money p-1 w-64 border text-main-text ml-8">Current balance: ${companyData.balance}</h2>
+              <h2 className="text-sm font-bold bg-bid-money p-1 w-64 border text-main-text ml-8">Current balance: ${missionData.budget}</h2>
               <p className="text-xl font-semibold leading-5 mt-8 ml-10 mr-3 text-main-text">Offer Description</p>
               <div className="flex flex-col px-1 py-5 mt-4 ml-8 mr-8 mb-4 w-128 bg-grey-bg rounded-xl">
                 <p className="text-sm font-semibold leading-5 ml-3 mr-3 text-sub-text">{bidData.description}</p>
